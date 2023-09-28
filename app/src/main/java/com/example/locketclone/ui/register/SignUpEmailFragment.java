1920c2c47d0a89fecf69d207424e3546f0600b5a
sadnessismybestestfriend <ndmquan1010@gmail.com>
2023-09-29 00:17:13 +0700

    update sign up ui

---
+++ app/src/main/java/com/example/locketclone/ui/register/SignUpEmailFragment.java
@@ -0,0 +1,29 @@
+package com.example.locketclone.ui.register;
+
+import android.view.LayoutInflater;
+
+import com.example.locketclone.base.BaseFragment;
+import com.example.locketclone.databinding.FragmentSignUpEmailBinding;
+
+public class SignUpEmailFragment extends BaseFragment<FragmentSignUpEmailBinding> {
+
+    @Override
+    public void initData() {
+
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public void initEvent() {
+
+    }
+
+    @Override
+    protected FragmentSignUpEmailBinding inflateViewBinding(LayoutInflater inflater) {
+        return FragmentSignUpEmailBinding.inflate(inflater);
+    }
+}
---
+++ app/src/main/java/com/example/locketclone/ui/register/SignUpPasswordFragment.java
@@ -0,0 +1,29 @@
+package com.example.locketclone.ui.register;
+
+import android.view.LayoutInflater;
+
+import com.example.locketclone.base.BaseFragment;
+import com.example.locketclone.databinding.FragmentSignUpPasswordBinding;
+
+public class SignUpPasswordFragment extends BaseFragment<FragmentSignUpPasswordBinding> {
+
+    @Override
+    public void initData() {
+
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public void initEvent() {
+
+    }
+
+    @Override
+    protected FragmentSignUpPasswordBinding inflateViewBinding(LayoutInflater inflater) {
+        return FragmentSignUpPasswordBinding.inflate(inflater);
+    }
+}
