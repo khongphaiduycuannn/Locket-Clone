1f050694fc9d625e916de486ae866309b09d9ab3
Duy Cuan <ndmquan1010@gmail.com>
2023-12-12 19:33:03 +0700

    update profile dialog ui

---
+++ app/src/main/java/com/example/locketclone/ui/profile/EditUsernameFragment.java
@@ -0,0 +1,29 @@
+package com.example.locketclone.ui.profile;
+
+import android.view.LayoutInflater;
+
+import com.example.locketclone.base.BaseFragment;
+import com.example.locketclone.databinding.FragmentEditUsernameBinding;
+
+public class EditUsernameFragment extends BaseFragment<FragmentEditUsernameBinding> {
+
+    @Override
+    public void initData() {
+
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public void initEvent() {
+
+    }
+
+    @Override
+    protected FragmentEditUsernameBinding inflateViewBinding(LayoutInflater inflater) {
+        return FragmentEditUsernameBinding.inflate(inflater);
+    }
+}
--- app/src/main/java/com/example/locketclone/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/locketclone/ui/profile/ProfileFragment.java
@@ -1,7 +1,15 @@
 package com.example.locketclone.ui.profile;
 
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.view.LayoutInflater;
+import android.view.Window;
+import android.view.WindowManager;
 
+import androidx.navigation.Navigation;
+
+import com.example.locketclone.R;
 import com.example.locketclone.base.BaseFragment;
 import com.example.locketclone.databinding.FragmentProfileBinding;
 
@@ -20,11 +28,31 @@ public class ProfileFragment extends BaseFragment<FragmentProfileBinding> {
 
     @Override
     public void initEvent() {
+        getBinding().btnEditUsername.setOnClickListener(view -> {
+            showEditAvatarDialog();
+        });
 
+        getBinding().btnEditAvatar.setOnClickListener(view -> {
+            Navigation.findNavController(getView()).navigate(R.id.action_profileFragment_to_editUsernameFragment);
+        });
     }
 
     @Override
     protected FragmentProfileBinding inflateViewBinding(LayoutInflater inflater) {
         return FragmentProfileBinding.inflate(inflater);
+    }
+
+    private void showEditAvatarDialog() {
+        Dialog settingDialog = new Dialog(requireContext());
+        settingDialog.setContentView(R.layout.dialog_edit_avatar);
+
+        Window window = settingDialog.getWindow();
+        if (window != null) {
+            window.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+            window.setLayout(
+                    WindowManager.LayoutParams.MATCH_PARENT,
+                    WindowManager.LayoutParams.WRAP_CONTENT);
+        }
+        settingDialog.show();
     }
 }
