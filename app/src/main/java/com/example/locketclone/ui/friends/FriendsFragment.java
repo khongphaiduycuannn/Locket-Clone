8f7ad497256549a1a5aa6501927e213574d0817b
Duy Cuan <ndmquan1010@gmail.com>
2023-12-12 20:46:38 +0700

    update friends ui

---
+++ app/src/main/java/com/example/locketclone/ui/friends/FriendsFragment.java
@@ -0,0 +1,29 @@
+package com.example.locketclone.ui.friends;
+
+import android.view.LayoutInflater;
+
+import com.example.locketclone.base.BaseFragment;
+import com.example.locketclone.databinding.FragmentFriendsBinding;
+
+public class FriendsFragment extends BaseFragment<FragmentFriendsBinding> {
+
+    @Override
+    public void initData() {
+
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public void initEvent() {
+
+    }
+
+    @Override
+    protected FragmentFriendsBinding inflateViewBinding(LayoutInflater inflater) {
+        return FragmentFriendsBinding.inflate(inflater);
+    }
+}
