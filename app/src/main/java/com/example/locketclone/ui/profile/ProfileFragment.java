5485e614264cabfc993e22d45ca5d8ba0eda6ee7
Duy Cuan <ndmquan1010@gmail.com>
2023-12-12 18:55:50 +0700

    update profile ui

---
+++ app/src/main/java/com/example/locketclone/ui/profile/ProfileFragment.java
@@ -0,0 +1,30 @@
+package com.example.locketclone.ui.profile;
+
+import android.view.LayoutInflater;
+
+import com.example.locketclone.base.BaseFragment;
+import com.example.locketclone.databinding.FragmentProfileBinding;
+
+public class ProfileFragment extends BaseFragment<FragmentProfileBinding> {
+
+
+    @Override
+    public void initData() {
+
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public void initEvent() {
+
+    }
+
+    @Override
+    protected FragmentProfileBinding inflateViewBinding(LayoutInflater inflater) {
+        return FragmentProfileBinding.inflate(inflater);
+    }
+}
