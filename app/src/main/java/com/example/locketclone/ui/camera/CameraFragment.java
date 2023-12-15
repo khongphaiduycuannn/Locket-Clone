9b3ec85627c1e44709933a45a5cec735a2fc64f4
Duy Cuan <ndmquan1010@gmail.com>
2023-12-15 16:10:22 +0700

    ui camera

---
+++ app/src/main/java/com/example/locketclone/ui/camera/CameraFragment.java
@@ -0,0 +1,29 @@
+package com.example.locketclone.ui.camera;
+
+import android.view.LayoutInflater;
+
+import com.example.locketclone.base.BaseFragment;
+import com.example.locketclone.databinding.FragmentCameraBinding;
+
+public class CameraFragment extends BaseFragment<FragmentCameraBinding> {
+
+    @Override
+    public void initData() {
+
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public void initEvent() {
+
+    }
+
+    @Override
+    protected FragmentCameraBinding inflateViewBinding(LayoutInflater inflater) {
+        return FragmentCameraBinding.inflate(inflater);
+    }
+}
